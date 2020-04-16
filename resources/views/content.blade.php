0b4565d240ca86381a3cad6cfbc738bdcf1356c5
vagrant <vagrant@homestead>
2020-04-16 08:03:08 +0000

    评论展示

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -53,144 +53,34 @@
 			<div class='article-header'>
 				<h5 class='h5'>文章评论</h5>
 			</div>
+			    @foreach($article->comment as $comments)
 				<div class='showcomment'>
-					<div class='small mt-2'>
+					<!-- 主评论 -->
+				
+					<div class='small mt-2' style='width:{{(100-($comments->level*10))+10}}%;margin-left: {{100-((100-($comments->level*10))+10)}}%'>
 					    <div>
-						    <img src="images/1563795007904.jpeg">
-						    <span>用户名</span>
+						    <img src="https://dss0.baidu.com/73x1bjeh1BF3odCf/it/u=970046986,3090048325&fm=85&s=4C12C519C8913CCA569E5FC2030080A8">
 						    <span>四川的网友</span>
-					        <time>2013-8-9</time>
+					        <time>{{$comments->created_at}}</time>
 					        <span class='float-right'>
-					    	    <b>1楼</b>
+					    	  
 					        </span>
 					        <div class='mentcon'>
+					        <p>{{$comments->content}}</p>
 					        <br><br>
 					        <ul>
 					        	<!-- 点赞 -->
-					       <li>
-  							   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thumbs-up"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
-  							   </svg>&nbsp;
-  							    (<span class='sup'>56</span>)
-							</li>
-                            <!-- 反对 -->
-							<li>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thumbs-down"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
-  							    (<span class='fan'>45</span>)
-							</li>
+					      
 							<!-- 回复 -->
-							<li class="callback" data-flag='0'>&nbsp;
-							    	 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-repeat"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg>
+							<li class="callback" data-flag='0' data-level='{{$comments->level}}' date-parent='{{$comments->parent_id}}' >&nbsp;
+							    	 回复
 							</li>
 						    </ul>
 					       </div>
 					    </div>
-
 				    </div>
-				    <div class='userment'>
-				    	<!-- 用户评论展示 子评论-->
-					    <div>
-						    <img src="./images/-741352d2e87aa1a4.jpg">
-						    <span>435</span>
-						    <span>四川的网友</span>
-					        <time>45-45432</time>
-					        <span class='float-rght'>
-					    	    <b>#324</b>
-					        </span>
-					        <div class='mentcon'>
-						     123
-					            <br><br>
-					            <!-- 子评论点赞 -->
-					           <div>
-					            	<span>支持（<span class='sm_zan' flag=1 sm_id=""></span>）</span>
-						           <span>反对（<span class='sm_zai' flag=1 sm_id=""></span>）</span>
-						           <span class='callback' data-flag='0'> 回复</span>
-					            </div>
-
-					       </div>
-					    </div>
-					  	<!-- 用户评论展示 子评论-->
-				    </div>
-				    <div class='userment' style='width:80%;margin-left: 19%'>
-				    	<!-- 用户评论展示 子评论-->
-					    <div>
-						    <img src="./images/-741352d2e87aa1a4.jpg">
-						    <span>435</span>
-						    <span>四川的网友</span>
-					        <time>45-45432</time>
-					        <span class='float-rght'>
-					    	    <b>#324</b>
-					        </span>
-					        <div class='mentcon'>
-						     @435你这不行啊
-					            <br><br>
-					            <!-- 子评论点赞 -->
-					            <div>
-					            	<span>支持（<span class='sm_zan' flag=1 sm_id=""></span>）</span>
-						           <span>反对（<span class='sm_zai' flag=1 sm_id=""></span>）</span>
-						           <span class='callback' data-flag='0'> 回复</span>
-					            </div>
-					           
-					       </div>
-					    </div>
-					  	<!-- 用户评论展示 子评论-->
-				    </div>
-				    <div class='userment' style='width:75%;margin-left: 24%'>
-				    	<!-- 用户评论展示 子评论-->
-					    <div>
-						    <img src="./images/-741352d2e87aa1a4.jpg">
-						    <span>435</span>
-						    <span>四川的网友</span>
-					        <time>45-45432</time>
-					        <span class='float-rght'>
-					    	    <b>#324</b>
-					        </span>
-					        <div class='mentcon'>
-						     @435你这不行啊
-					            <br><br>
-					            <!-- 子评论点赞 -->
-					           <div>
-					            	<span>支持（<span class='sm_zan' flag=1 sm_id=""></span>）</span>
-						           <span>反对（<span class='sm_zai' flag=1 sm_id=""></span>）</span>
-						           <span class='callback' data-flag='0'> 回复</span>
-					            </div>
-					          
-					       </div>
-					    </div>
-					  	<!-- 用户评论展示 子评论-->
-				    </div>
-				    <div class='small mt-2'>
-					    <div>
-						    <img src="images/1563795007904.jpeg">
-						    <span>用户名</span>
-						    <span>四川的网友</span>
-					        <time>2013-8-9</time>
-					        <span class='float-right'>
-					    	    <b>1楼</b>
-					        </span>
-					        <div class='mentcon'>
-					        <br><br>
-					        <ul>
-					        	<!-- 点赞 -->
-					       <li>
-  							   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thumbs-up"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
-  							   </svg>&nbsp;
-  							    (<span class='sup'>56</span>)
-							</li>
-                            <!-- 反对 -->
-							<li>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thumbs-down"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>
-  							    (<span class='fan'>45</span>)
-							</li>
-							<!-- 回复 -->
-							<li class="callback" flag='0'>&nbsp;
-							    	 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-repeat"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg>
-							</li>
-						    </ul>
-					       </div>
-					    </div>
-				    </div> 
-			
 				</div>
+				@endforeach
 		    
 		<!-- 评论end -->
 		<form class='mt-3'><div class='form-group'><textarea class='form-control' name='conment' placeholder='说两句吧~'></textarea></div> <div class='form-group row'><img src="https://blog.yzmcms.com/api/index/code.html???????css/" class='ml-3' style='height: 40px;'>
@@ -199,6 +89,13 @@
 		</div>
 	    @include('layouts._aside')
 	</div>
+	<!-- 隐藏域 -->
+
 @endsection
+<script type="text/javascript">
+	$(function(){
+		//获取
+	})
+</script>
 </body>
 </html>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,11 +10,10 @@
 		    <ul class="navbar-nav mr-auto">
 		    	@foreach($datas as $data)
 		        <li class="nav-item active">
-		      	@if($data->cate_name!='关于我')
+		      	@if($data->cate_name!='关于我' && $data->cate_name!='文章归档')
 		        <a class="nav-link text-nowrap" href="{{route('cates.show',[$data->id])}}">{{$data->cate_name}}<span class="sr-only">(current)</span></a>
 		        @elseif($data->cate_name=='关于我')
 		         <a class="nav-link text-nowrap" href="{{route('articles.show',[$data->id])}}">{{$data->cate_name}}<span class="sr-only">(current)</span></a>
-		         
 		        @elseif($data->cate_name=='文章归档')
 		        <a class="nav-link text-nowrap" href="{{url('/files')}}">{{$data->cate_name}}<span class="sr-only">(current)</span></a>
 		        @endif
